d9f8c3fdda23aea909768cb8bbb8f7bb7409bb98
Deagozn <124474840+Deagozn@users.noreply.github.com>
2023-06-27 12:23:11 +0800

    dashboard updated

--- app/src/main/java/com/example/project/Dashboard.java
+++ app/src/main/java/com/example/project/Dashboard.java
@@ -9,6 +9,6 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard); //chester
+        setContentView(R.layout.dashboard);
     }
 }
