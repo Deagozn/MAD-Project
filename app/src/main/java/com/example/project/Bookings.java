214d1cc14c4e6ffe96650b7c8d0ee9e05fcb4187
Deagozn <Deagozn@users.noreply.github.com>
2023-06-28 19:16:04 +0800

    bookings page added tabs and time setting

--- app/src/main/java/com/example/project/Bookings.java
+++ app/src/main/java/com/example/project/Bookings.java
@@ -2,13 +2,94 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TabHost;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class Bookings extends AppCompatActivity {
+
+    private TabHost tabHost;
+    private ImageButton back_button;
+    private Button start_hours;
+    private int start_hour, start_minute;
+    private Button end_hours;
+    private int end_hour, end_minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookings);
+        setContentView(R.layout.bookings);
+
+        start_hours = findViewById(R.id.start_time);
+        end_hours = findViewById(R.id.end_time);
+
+        back_button = findViewById(R.id.back_bookings);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Bookings.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        tabHost = findViewById(R.id.booking_tabs);
+        tabHost.setup();
+
+        // New
+        TabHost.TabSpec spec = tabHost.newTabSpec("New");
+        spec.setContent(R.id.New);
+        spec.setIndicator("New");
+        tabHost.addTab(spec);
+
+        // Existing
+        spec = tabHost.newTabSpec("Existing");
+        spec.setContent(R.id.Existing);
+        spec.setIndicator("Existing");
+
+        tabHost.addTab(spec);
+    }
+
+    public void popTimePickerStart(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour1, int selectedMinute1) {
+                start_hour = selectedHour1;
+                start_minute = selectedMinute1;
+                start_hours.setText(String.format(Locale.getDefault(), "%02d:%02d", start_hour, start_minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, start_hour, start_minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
+    public void popTimePickerEnd(View view) {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour1, int selectedMinute1) {
+                end_hour = selectedHour1;
+                end_minute = selectedMinute1;
+                end_hours.setText(String.format(Locale.getDefault(), "%02d:%02d", end_hour, end_minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, end_hour, end_minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 }
--- app/src/main/java/com/example/project/SplashActivity.java
+++ app/src/main/java/com/example/project/SplashActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ImageView;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
