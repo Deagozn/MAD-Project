48b8460fa08a53482b9c1a6574aa0cc039b61e15
Chester <Chestern2121@gmail.com>
2023-06-29 16:25:58 +0800

    Library finder back button

--- app/src/main/java/com/example/project/LibraryFinder.java
+++ app/src/main/java/com/example/project/LibraryFinder.java
@@ -2,13 +2,26 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LibraryFinder extends AppCompatActivity {
+    ImageView back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.library_finder);
+        back_button=findViewById(R.id.back_finder);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(LibraryFinder.this,Dashboard.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
