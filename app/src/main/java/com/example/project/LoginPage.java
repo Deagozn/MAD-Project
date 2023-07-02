9d184f3bbbc8bcc44c39b83aa3ecec979c18d9e9
Deagozn <Deagozn@users.noreply.github.com>
2023-07-02 17:08:04 +0800

    added ForgetPasswordPt1 with login page connection

--- app/src/main/java/com/example/project/LoginPage.java
+++ app/src/main/java/com/example/project/LoginPage.java
@@ -2,13 +2,26 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
+    Button forgetpw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        forgetpw = findViewById(R.id.forget_pw);
+        forgetpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, ForgetPasswordPt1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
