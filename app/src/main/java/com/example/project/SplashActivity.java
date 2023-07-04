2f8d186b5e067a851cb3e4e0efcfc6a682c337b7
Catfishrofl <nicholasleewx@outlook.com>
2023-07-04 11:11:26 +0800

    added audio to splash

--- app/src/main/java/com/example/project/SplashActivity.java
+++ app/src/main/java/com/example/project/SplashActivity.java
@@ -1,17 +1,23 @@
 package com.example.project;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
-
 public class SplashActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        // Initialize MediaPlayer and load the sound file
+        mediaPlayer = MediaPlayer.create(this, R.raw.splash_sound);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,5 +29,25 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 6300);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Start playing the sound effect when the activity is resumed
+        if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Pause and release the MediaPlayer when the activity is paused
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
